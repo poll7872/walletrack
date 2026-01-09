135da0ea7985c4b11c6a2fbd78619c7d8f385901
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-08 21:22:07 -0500

    Add expense management controller

---
+++ backend/src/controllers/ExpenseController.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from "express";
+import Expense from "../models/Expense";
+
+export class ExpensesController {
+  static create = async (req: Request, res: Response) => {
+    try {
+      const expense = new Expense(req.body);
+      expense.budgetId = req.budget?.id;
+      await expense.save();
+      res
+        .status(201)
+        .json({ message: "Expense created successfully", expense });
+    } catch (error) {
+      res.status(500).json({ error: "Error creating expense" });
+    }
+  };
+
+  static getById = async (req: Request, res: Response) => {
+    res.status(200).json(req.expense);
+  };
+
+  static updateById = async (req: Request, res: Response) => {
+    await req.expense!.update(req.body);
+    res.status(200).json({ message: "Expense updated successfully" });
+  };
+
+  static deleteById = async (req: Request, res: Response) => {
+    await req.expense!.destroy();
+    res.status(200).json({ message: "Expense deleted successfully" });
+  };
+}
