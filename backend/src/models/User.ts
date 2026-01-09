63ace161d6bf4095c494e6ebf6c6d3e1ea8bd491
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-08 21:23:38 -0500

    Add user data model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,54 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  HasMany,
+  Default,
+  Unique,
+  AllowNull,
+} from "sequelize-typescript";
+import Budget from "./Budget";
+
+@Table({
+  tableName: "users",
+})
+export class User extends Model {
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(60),
+  })
+  declare name: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(60),
+  })
+  declare password: string;
+
+  @Unique(true)
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING(50),
+  })
+  declare email: string;
+
+  @Column({
+    type: DataType.STRING(6),
+  })
+  declare token: string;
+
+  @Default(false)
+  @Column({
+    type: DataType.BOOLEAN,
+  })
+  declare confirmed: boolean;
+
+  @HasMany(() => Budget, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  declare budgets: Budget[];
+}
+
+export default User;
