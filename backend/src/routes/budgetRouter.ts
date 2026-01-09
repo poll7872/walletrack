38d008733d5e915366bb4ce16ccad308593f0396
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-08 21:21:01 -0500

    Update budget router with authentication middleware

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -6,11 +6,20 @@ import {
   validateBudgetExists,
   validateBudgetInput,
 } from "../middleware/budget";
+import { ExpensesController } from "../controllers/ExpenseController";
+import {
+  validateExpenseExists,
+  validateExpenseId,
+  validateExpenseInput,
+} from "../middleware/expense";
 
 const router = Router();
 
 router.param("budgetId", validateBudgetId);
 router.param("budgetId", validateBudgetExists);
+
+router.param("expenseId", validateExpenseId);
+router.param("expenseId", validateExpenseExists);
 
 router.get("/", BudgetController.getAll);
 
@@ -31,5 +40,21 @@ router.put(
 );
 
 router.delete("/:budgetId", BudgetController.deleteById);
+
+/** Routes for Expense */
+router.post(
+  "/:budgetId/expenses",
+  validateExpenseInput,
+  handleInputErrors,
+  ExpensesController.create,
+);
+router.get("/:budgetId/expenses/:expenseId", ExpensesController.getById);
+router.put(
+  "/:budgetId/expenses/:expenseId",
+  validateExpenseInput,
+  handleInputErrors,
+  ExpensesController.updateById,
+);
+router.delete("/:budgetId/expenses/:expenseId", ExpensesController.deleteById);
 
 export default router;
