17887a4249947ad93a7dc01bb93884bbce1098d6
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-14 21:24:44 -0500

    Update BudgetController.ts implementation

--- backend/src/controllers/BudgetController.ts
+++ backend/src/controllers/BudgetController.ts
@@ -19,7 +19,7 @@ export class BudgetController {
 
   static create = async (req: Request, res: Response) => {
     try {
-      const budget = new Budget(req.body);
+      const budget = await Budget.create(req.body);
       budget.userId = req.user?.id;
       await budget.save();
       res.status(201).json({ message: "budget created successfully", budget });
