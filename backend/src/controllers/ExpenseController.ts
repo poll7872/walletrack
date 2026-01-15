a6b794c18e95d74e376c93fa55cbea0856ffe6ac
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-14 21:24:46 -0500

    Update ExpenseController.ts implementation

--- backend/src/controllers/ExpenseController.ts
+++ backend/src/controllers/ExpenseController.ts
@@ -4,7 +4,7 @@ import Expense from "../models/Expense";
 export class ExpensesController {
   static create = async (req: Request, res: Response) => {
     try {
-      const expense = new Expense(req.body);
+      const expense = await Expense.create(req.body);
       expense.budgetId = req.budget?.id;
       await expense.save();
       res
