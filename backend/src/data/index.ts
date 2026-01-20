25c7168832f741b0ede8de8b4aba7fcc1e643b58
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-19 20:45:12 -0500

    Add data directory for test database setup

---
+++ backend/src/data/index.ts
@@ -0,0 +1,16 @@
+import { exit } from "node:process";
+import db from "../config/db";
+
+const clearData = async () => {
+  try {
+    await db.sync({ force: true });
+    exit(0);
+  } catch (error) {
+    //console.log(error)
+    exit(1);
+  }
+};
+
+if (process.argv[2] === "--clear") {
+  clearData();
+}
