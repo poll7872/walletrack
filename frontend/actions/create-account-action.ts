9cee43e6077c3b739f7b68a6606eb052d1ee808c
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-23 20:51:03 -0500

    Add actions and src directories with server actions and utilities

---
+++ frontend/actions/create-account-action.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { RegisterSchema } from "@/src/schemas";
+
+type ActionStateType = {
+  errors: string[];
+};
+
+export async function register(prevState: ActionStateType, formData: FormData) {
+  const registerData = {
+    email: formData.get("email"),
+    name: formData.get("name"),
+    password: formData.get("password"),
+    password_confirmation: formData.get("password_confirmation"),
+  };
+
+  // validate
+  const register = RegisterSchema.safeParse(registerData);
+
+  // register user
+  if (!register.success) {
+    const errors = register.error.issues.map((error) => error.message);
+    return { errors };
+  }
+
+  const url = `${process.env.API_URL}/auth/create-account`;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: register.data.name,
+      password: register.data.password,
+      email: register.data.email,
+    }),
+  });
+
+  const json = await req.json();
+  console.log(json);
+  return {
+    errors: [],
+  };
+}
---
+++ frontend/src/schemas/index.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const RegisterSchema = z
+  .object({
+    name: z.string().min(3, { error: "El nombre es obligatorio" }).max(50),
+    email: z
+      .email({ error: "El email es invalido" })
+      .min(1, { error: "El email es obligatorio" }),
+    password: z
+      .string()
+      .min(8, { error: "La contraseña debe tener min 8 caracteres" })
+      .max(50),
+    password_confirmation: z
+      .string()
+      .min(8, { error: "La contraseña debe tener min 8 caracteres" })
+      .max(50),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    error: "Las contraseñas no coinciden",
+    path: ["password_confirmation"],
+  });
