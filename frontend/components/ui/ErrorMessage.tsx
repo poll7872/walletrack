4ec4da730612d2c07f086eb3f30bfa989af882c0
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-26 21:41:49 -0500

    Add error message UI component

---
+++ frontend/components/ui/ErrorMessage.tsx
@@ -0,0 +1,3 @@
+export const ErrorMessage = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-red-500 text-white font-bold p-3 text-center rounded-lg">{children}</div>;
+};
