a16ae0a295f79f1d3aa4bd48bc77743b7ba8a895
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-26 21:41:02 -0500

    Add account confirmation action with token validation

---
+++ frontend/actions/confirm-account-action.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { ErrorResponseSchema, SuccessSchema, TokenSchema } from "@/src/schemas";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export async function confirmAccount(
+  prevState: ActionStateType,
+  formData: FormData,
+): Promise<ActionStateType> {
+  const token = formData.get("token");
+  const confirmToken = TokenSchema.safeParse(token);
+  if (!confirmToken.success) {
+    return {
+      errors: confirmToken.error.issues.map((error) => error.message),
+      success: "",
+    };
+  }
+
+  //confirmar usuario
+  const url = `${process.env.API_URL}/auth/confirm-account`;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: confirmToken.data,
+    }),
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const { message } = SuccessSchema.parse(json);
+  return {
+    errors: [],
+    success: message,
+  };
+}
