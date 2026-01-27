a89c3253fe85e78c576f945ed3de43e040792531
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-26 21:41:54 -0500

    Add success message UI component

---
+++ frontend/components/ui/SuccessMessage.tsx
@@ -0,0 +1,3 @@
+export const SuccessMessage = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-green-500 text-white font-bold p-3 text-center rounded-lg">{children}</div>;
+};
