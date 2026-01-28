d04b8899d1770323c43746b34d40a64d03ffc38a
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-27 21:19:52 -0500

    Add auth schemas: Login, Token, ForgotPassword, User

--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { id } from "zod/locales";
 
 export const RegisterSchema = z
   .object({
@@ -17,6 +18,23 @@ export const RegisterSchema = z
     path: ["password_confirmation"],
   });
 
+export const LoginSchema = z.object({
+  email: z
+    .email({ error: "El email es invalido" })
+    .min(1, { error: "El email es obligatorio" }),
+  password: z.string().min(1, { error: "La contraseña es obligatoria" }),
+});
+
+export const TokenSchema = z
+  .string({ error: "Token invalido" })
+  .length(6, { error: "Token invalido" });
+
+export const ForgotPasswordSchema = z.object({
+  email: z
+    .email({ error: "Email no válido" })
+    .min(1, { error: "El Email es Obligatorio" }),
+});
+
 export const SuccessSchema = z.object({
   message: z.string(),
 });
@@ -25,6 +43,10 @@ export const ErrorResponseSchema = z.object({
   error: z.string(),
 });
 
-export const TokenSchema = z
-  .string({ error: "Token invalido" })
-  .length(6, { error: "Token invalido" });
+export const UserSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  email: z.email(),
+});
+
+export type User = z.infer<typeof UserSchema>;
