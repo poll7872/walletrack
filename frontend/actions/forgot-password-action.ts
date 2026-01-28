1fb815f2ebc501ee2d519ceb64f018a2fca98f04
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-27 21:20:29 -0500

    Add forgot password action

---
+++ frontend/actions/forgot-password-action.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import {
+  ErrorResponseSchema,
+  ForgotPasswordSchema,
+  SuccessSchema,
+} from "@/src/schemas";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export async function forgotPassword(
+  prevState: ActionStateType,
+  formData: FormData,
+) {
+  const forgotPassword = ForgotPasswordSchema.safeParse({
+    email: formData.get("email"),
+  });
+
+  if (!forgotPassword.success) {
+    return {
+      errors: forgotPassword.error.issues.map((error) => error.message),
+      success: "",
+    };
+  }
+
+  const url = `${process.env.API_URL}/auth/forgot-password `;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: forgotPassword.data.email,
+    }),
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const success = SuccessSchema.parse(json).message;
+  return {
+    errors: [],
+    success,
+  };
+}
