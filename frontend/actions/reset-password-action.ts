3e17b553f1be723a2b9c89583a37596d7a0614a6
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-28 21:14:19 -0500

    Add reset password action

---
+++ frontend/actions/reset-password-action.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import {
+  ErrorResponseSchema,
+  ResetPasswordSchema,
+  SuccessSchema,
+} from "@/src/schemas";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export async function resetPassword(
+  token: string,
+  prevState: ActionStateType,
+  formData: FormData,
+) {
+  const resetPasswordInput = {
+    password: formData.get("password"),
+    password_confirmation: formData.get("password_confirmation"),
+  };
+
+  const resetPassword = ResetPasswordSchema.safeParse(resetPasswordInput);
+  if (!resetPassword.success) {
+    return {
+      errors: resetPassword.error.issues.map((error) => error.message),
+      success: "",
+    };
+  }
+
+  const url = `${process.env.API_URL}/auth/reset-password/${token}`;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: resetPassword.data.password,
+    }),
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const { message } = SuccessSchema.parse(json);
+  return {
+    errors: [],
+    success: message,
+  };
+}
