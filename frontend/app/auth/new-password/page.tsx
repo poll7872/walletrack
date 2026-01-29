5f7615e1cb2e5d305b3c3997f03344ece0be57a5
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-28 21:13:13 -0500

    Update new password page with UI and handler

--- frontend/app/auth/new-password/page.tsx
+++ frontend/app/auth/new-password/page.tsx
@@ -1,3 +1,15 @@
+import { PasswordResetHandler } from "@/components/auth/PasswordResetHandler";
+
 export default function NewPaswordPage() {
-  return <div>NewPaswordPage</div>;
+  return (
+    <>
+      <h1 className="text-4xl font-black text-slate-800">
+        Reestablecer Contraseña
+      </h1>
+      <p className="text-lg text-slate-600 mt-2">
+        Ingresa el código que recibiste por email
+      </p>
+      <PasswordResetHandler />
+    </>
+  );
 }
