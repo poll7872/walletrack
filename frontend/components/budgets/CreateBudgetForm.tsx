8c498fff0e46b75b7c7141899858b31db9b03b57
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-30 21:06:25 -0500

    Refactor CreateBudgetForm to use BudgetForm component

--- frontend/components/budgets/CreateBudgetForm.tsx
+++ frontend/components/budgets/CreateBudgetForm.tsx
@@ -5,6 +5,7 @@ import { useActionState, useEffect } from "react";
 import { ErrorMessage } from "../ui/ErrorMessage";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+import { BudgetForm } from "./BudgetForm";
 
 export const CreateBudgetForm = () => {
   const router = useRouter();
@@ -36,36 +37,7 @@ export const CreateBudgetForm = () => {
           ))}
         </div>
       )}
-      <div className="relative">
-        <input
-          id="name"
-          className="block px-3.5 pb-2.5 pt-4 w-full text-base text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
-          type="text"
-          placeholder=" "
-          name="name"
-        />
-        <label
-          htmlFor="name"
-          className="absolute text-base text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-focus:px-2 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
-          Nombre del Presupuesto
-        </label>
-      </div>
-      <div className="relative">
-        <input
-          type="number"
-          id="amount"
-          className="block px-3.5 pb-2.5 pt-4 w-full text-base text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
-          placeholder=" "
-          name="amount"
-        />
-        <label
-          htmlFor="amount"
-          className="absolute text-base text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-focus:px-2 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
-          Cantidad del Presupuesto
-        </label>
-      </div>
+      <BudgetForm />
       <input
         type="submit"
         className="bg-green-600 hover:bg-green-700 w-full p-3 rounded-lg text-white font-bold text-lg cursor-pointer"
