d5271f8811276afb1b00f3f174a1c00f09779129
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-30 21:07:04 -0500

    Add reusable BudgetForm component for budget creation and editing

---
+++ frontend/components/budgets/BudgetForm.tsx
@@ -0,0 +1,40 @@
+import { Budget } from "@/src/schemas";
+
+export const BudgetForm = ({ budget }: { budget?: Budget }) => {
+  return (
+    <>
+      <div className="relative">
+        <input
+          id="name"
+          className="block px-3.5 pb-2.5 pt-4 w-full text-base text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
+          type="text"
+          placeholder=" "
+          name="name"
+          defaultValue={budget?.name}
+        />
+        <label
+          htmlFor="name"
+          className="absolute text-base text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-focus:px-2 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+        >
+          Nombre del Presupuesto
+        </label>
+      </div>
+      <div className="relative">
+        <input
+          type="number"
+          id="amount"
+          className="block px-3.5 pb-2.5 pt-4 w-full text-base text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer"
+          placeholder=" "
+          name="amount"
+          defaultValue={budget?.amount}
+        />
+        <label
+          htmlFor="amount"
+          className="absolute text-base text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 bg-white px-2 peer-focus:px-2 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+        >
+          Cantidad del Presupuesto
+        </label>
+      </div>
+    </>
+  );
+};
