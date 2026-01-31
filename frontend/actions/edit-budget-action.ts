97e27396eccb6f4d46ad1672277cdf26e54c7bdd
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-01-30 21:06:28 -0500

    Add edit budget action for budget management

---
+++ frontend/actions/edit-budget-action.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { getToken } from "@/src/auth/token";
+import {
+  Budget,
+  DraftBudgetSchema,
+  ErrorResponseSchema,
+  SuccessSchema,
+} from "@/src/schemas";
+import { revalidateTag } from "next/cache";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export async function editBudget(
+  budgetId: Budget["id"],
+  prevState: ActionStateType,
+  formData: FormData,
+) {
+  const budgetData = {
+    name: formData.get("name"),
+    amount: formData.get("amount"),
+  };
+
+  const budget = DraftBudgetSchema.safeParse(budgetData);
+  if (!budget.success) {
+    return {
+      errors: budget.error.issues.map((issue) => issue.message),
+      success: "",
+    };
+  }
+
+  const token = await getToken();
+  const url = `${process.env.API_URL}/budgets/${budgetId}`;
+  const req = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      name: budget.data.name,
+      amount: budget.data.amount,
+    }),
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const { message } = SuccessSchema.parse(json);
+
+  return {
+    errors: [],
+    success: message,
+  };
+}
