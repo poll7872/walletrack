7cd40e3a2381744e726ba88d9acff5c2e05d6ec9
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-02 21:47:20 -0500

    Add margin bottom to ErrorMessage component

--- frontend/components/ui/ErrorMessage.tsx
+++ frontend/components/ui/ErrorMessage.tsx
@@ -1,3 +1,7 @@
 export const ErrorMessage = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-500 text-white font-bold p-3 text-center rounded-lg">{children}</div>;
+  return (
+    <div className="bg-red-500 text-white font-bold p-3 text-center rounded-lg mb-2">
+      {children}
+    </div>
+  );
 };
