a0ae82cf87b283ad8f8b52e0e6aed2faa8564a10
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-03 19:29:22 -0500

    Implement EditExpenseForm component

--- frontend/components/expenses/EditExpenseForm.tsx
+++ frontend/components/expenses/EditExpenseForm.tsx
@@ -1,3 +1,24 @@
-export const EditExpenseForm = () => {
-  return <div></div>;
+import { DialogTitle } from "@headlessui/react";
+import { ExpenseForm } from "./ExpenseForm";
+
+export const EditExpenseForm = ({ closeModal }: { closeModal: () => void }) => {
+  return (
+    <>
+      <DialogTitle as="h3" className="font-black text-4xl text-slate-800 my-5">
+        Editar Gasto
+      </DialogTitle>
+      <p className="text-xl font-bold">
+        Edita los detalles de un {""}
+        <span className="text-green-600">gasto</span>
+      </p>
+      <form className="shadow-md rounded-lg p-10 mt-10 space-y-6" noValidate>
+        <ExpenseForm />
+        <input
+          type="submit"
+          className="bg-green-600 hover:bg-green-700 w-full p-3 rounded-lg text-white font-bold text-lg cursor-pointer"
+          value="Guardar Cambios"
+        />
+      </form>
+    </>
+  );
 };
