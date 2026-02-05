e6e54f49f58504a01d9234eebd76f122e85fd618
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-04 21:05:30 -0500

    Add delete expense action function

---
+++ frontend/actions/delete-expense-action.ts
@@ -0,0 +1,50 @@
+"use server";
+
+import { getToken } from "@/src/auth/token";
+import {
+  Budget,
+  ErrorResponseSchema,
+  Expense,
+  SuccessSchema,
+} from "@/src/schemas";
+
+type BudgetAndExpenseId = {
+  budgetId: Budget["id"];
+  expenseId: Expense["id"];
+};
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export default async function deleteExpense(
+  { budgetId, expenseId }: BudgetAndExpenseId,
+  prevState: ActionStateType,
+  formData: FormData,
+) {
+  const token = await getToken();
+  const url = `${process.env.API_URL}/budgets/${budgetId}/expenses/${expenseId}`;
+  const req = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const { message } = SuccessSchema.parse(json);
+
+  return {
+    errors: [],
+    success: message,
+  };
+}
