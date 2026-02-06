9d82a5b180094a96f297592dabee9541f8aa2118
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-05 19:30:17 -0500

    Update ModalContainer to use new Headless UI v2 syntax

--- frontend/components/ui/ModalContainer.tsx
+++ frontend/components/ui/ModalContainer.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { Fragment } from "react";
 import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import {
   Dialog,
   DialogPanel,
-  Transition,
-  TransitionChild,
+  DialogBackdrop,
 } from "@headlessui/react";
 import { AddExpenseForm } from "../expenses/AddExpenseForm";
 import { EditExpenseForm } from "../expenses/EditExpenseForm";
@@ -47,42 +45,22 @@ export const ModalContainer = () => {
   };
 
   return (
-    <>
-      <Transition appear show={show} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/60" />
-          </TransitionChild>
+    <Dialog open={show} onClose={closeModal} className="relative z-50">
+      <DialogBackdrop 
+        transition
+        className="fixed inset-0 bg-black/60 duration-300 ease-out data-closed:opacity-0" 
+      />
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <DialogPanel className="w-full max-w-5xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                  {ComponentToRender ? (
-                    <ComponentToRender closeModal={closeModal} />
-                  ) : null}
-                </DialogPanel>
-              </TransitionChild>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel
+          transition
+          className="w-full max-w-5xl duration-300 ease-out data-closed:scale-95 data-closed:opacity-0 transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl p-16"
+        >
+          {ComponentToRender ? (
+            <ComponentToRender closeModal={closeModal} />
+          ) : null}
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 };
