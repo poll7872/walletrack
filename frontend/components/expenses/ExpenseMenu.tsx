a916a7ae0d0c60d37af188c9bd843c1bd6d32e0f
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-05 19:30:14 -0500

    Update ExpenseMenu to use new Headless UI v2 syntax

--- frontend/components/expenses/ExpenseMenu.tsx
+++ frontend/components/expenses/ExpenseMenu.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { Fragment } from "react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import {
   Menu,
   MenuButton,
   MenuItem,
   MenuItems,
-  Transition,
 } from "@headlessui/react";
 import { useRouter } from "next/navigation";
 import { Expense } from "@/src/schemas";
@@ -16,46 +14,39 @@ export const ExpenseMenu = ({ expenseId }: { expenseId: Expense["id"] }) => {
 
   return (
     <div className="flex shrink-0 items-center gap-x-6">
-      <Menu as="div" className="relative flex-none">
+      <Menu>
         <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
           <span className="sr-only">opciones</span>
           <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
         </MenuButton>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+        <MenuItems 
+          anchor="bottom"
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
         >
-          <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-            <MenuItem>
-              <button
-                type="button"
-                className="block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer"
-                onClick={() =>
-                  router.push(`?showModal=true&editExpenseId=${expenseId}`)
-                }
-              >
-                Editar Gasto
-              </button>
-            </MenuItem>
+          <MenuItem>
+            <button
+              type="button"
+              className="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900 data-focus:bg-gray-50"
+              onClick={() =>
+                router.push(`?showModal=true&editExpenseId=${expenseId}`)
+              }
+            >
+              Editar Gasto
+            </button>
+          </MenuItem>
 
-            <MenuItem>
-              <button
-                type="button"
-                className="block px-3 py-1 text-sm leading-6 text-red-500 cursor-pointer"
-                onClick={() =>
-                  router.push(`?showModal=true&deleteExpenseId=${expenseId}`)
-                }
-              >
-                Eliminar Gasto
-              </button>
-            </MenuItem>
-          </MenuItems>
-        </Transition>
+          <MenuItem>
+            <button
+              type="button"
+              className="block w-full px-3 py-1 text-left text-sm leading-6 text-red-500 data-focus:bg-red-50"
+              onClick={() =>
+                router.push(`?showModal=true&deleteExpenseId=${expenseId}`)
+              }
+            >
+              Eliminar Gasto
+            </button>
+          </MenuItem>
+        </MenuItems>
       </Menu>
     </div>
   );
