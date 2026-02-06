01a6e629505e52930b7e7274200842a0e08aa2aa
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-05 19:29:54 -0500

    Add progress bar and amount display to budget details page

--- frontend/app/admin/budgets/[id]/page.tsx
+++ frontend/app/admin/budgets/[id]/page.tsx
@@ -1,5 +1,7 @@
+import { ProgressBar } from "@/components/budgets/ProgressBar";
 import { AddExpenseButton } from "@/components/expenses/AddExpenseButton";
 import { ExpenseMenu } from "@/components/expenses/ExpenseMenu";
+import { Amount } from "@/components/ui/Amount";
 import { ModalContainer } from "@/components/ui/ModalContainer";
 import { getBudget } from "@/src/services/budgets";
 import { formatCurrency, formatDate } from "@/src/utils";
@@ -27,6 +29,15 @@ export default async function BudgetDetailsPage({
 }) {
   const { id } = await params;
   const budget = await getBudget(id);
+
+  const totalSpent = budget.expenses.reduce(
+    (total, expense) => +expense.amount + total,
+    0,
+  );
+
+  const percentage = +((totalSpent / +budget.amount) * 100).toFixed(2);
+
+  const totalAvailable = +budget.amount - totalSpent;
   return (
     <>
       <div className="flex justify-between items-center">
@@ -41,6 +52,15 @@ export default async function BudgetDetailsPage({
 
       {budget.expenses.length ? (
         <>
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-10">
+            <ProgressBar percentage={percentage} />
+            <div className="flex flex-col justify-center items-center md:items-start gap-2">
+              <Amount label="Presupuesto" amount={+budget.amount} />
+              <Amount label="Disponible" amount={totalAvailable} />
+              <Amount label="Gastado" amount={totalSpent} />
+            </div>
+          </div>
+
           <h1 className="font-black text-4xl text-slate-800 mt-10">
             Gastos en este presupuesto
           </h1>
