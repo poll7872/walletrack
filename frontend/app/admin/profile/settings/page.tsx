dfde78b8e410794baedd2b6afa63f0a0a6714e0b
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-06 20:38:01 -0500

    Add profile settings page

---
+++ frontend/app/admin/profile/settings/page.tsx
@@ -0,0 +1,19 @@
+import ProfileForm from "@/components/profile/ProfileForm";
+import { verifySession } from "@/src/auth/dal";
+
+export default async function EditProfilePage() {
+  const { user } = await verifySession();
+  return (
+    <>
+      <h1 className="font-black text-4xl text-slate-800 my-5">
+        Actualizar Perfil
+      </h1>
+      <p className="text-xl font-bold text-slate-600">
+        Aquí puedes cambiar los datos de tu {""}
+        <span className="text-green-600">perfil</span>
+      </p>
+
+      <ProfileForm user={user} />
+    </>
+  );
+}
